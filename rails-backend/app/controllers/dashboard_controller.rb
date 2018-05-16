f0c610fa6763ba6bc7177afc1cb9cc6c8c7cfaf4
sukhbir-singh <sukhbir947@gmail.com>
2018-05-16 21:16:49 +0530

    dashboard-chart turns dynamic

--- rails-backend/app/controllers/dashboard_controller.rb
+++ rails-backend/app/controllers/dashboard_controller.rb
@@ -9,15 +9,37 @@ class DashboardController < ApplicationController
 	def get_valid_votes
 		vote_ids = Vote.where({is_valid: true}).only(:leader_id)
 		leader_ids = vote_ids.map {|vote| Leader.find(vote.leader_id).id.to_s rescue 'null'}			
-		leaders = Leader.where(:_id.in => leader_ids).all
 
-		output = []
+		party = {}
+		leader_name = {}
 
-		leaders.each do |leader|
-			obj = {name: leader.name, id: leader.id.to_s, party: leader.party}
-			output.push(obj)
+		leader_ids.each do |l_id|
+			leader = Leader.find(l_id) rescue nil
+			next if leader.blank?
+
+			if party[leader.party].blank?
+				party[leader.party] = 1
+			else
+				party[leader.party] = party[leader.party] + 1
+			end
+
+			key = leader.name + " (" + leader.party + ")"
+
+			if leader_name[key].blank?
+				leader_name[key] = 1
+			else
+				leader_name[key] = leader_name[key] + 1
+			end
 		end
 
-		render json: {"success": true, data: output}
+		formatted_leader_names = leader_name.keys
+		formatted_counts = leader_name.values
+		formatted_party_names = party.keys
+		formatted_party_counts = party.values
+
+		render json: {"success": true, "data": {
+				leader_names: leader_name, party: party
+			}, "graph_data": { leaders: formatted_leader_names, leader_counts: formatted_counts, 
+				party: formatted_party_names, party_counts: formatted_counts } }
 	end
 end
